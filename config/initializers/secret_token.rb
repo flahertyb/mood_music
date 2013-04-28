e3096d5c884561bc24b49fa0e880e84fc08c9f6f
Bart Flaherty <bart@swipely.com>
2013-04-28 13:30:54 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoodMusic::Application.config.secret_token = 'd995d60c96b4aff1a6209cd6ca06d9f6f46a7d4b22192b43b0a64495e24f0c60de3acce3221951678b72bc2cbd96ea48dbf0b14b274b92955963be466836ed4f'
